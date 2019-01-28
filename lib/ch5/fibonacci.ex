dc2a860ab23316d3a58c8735a05d7728e1568c75
Tony Baines <tony.baines.1@gmail.com>
2019-01-28 09:09:43 +0000

    Refactor

--- lib/ch5/fibonacci.ex
+++ lib/ch5/fibonacci.ex
@@ -3,8 +3,11 @@ defmodule Fibonacci do
   def of(0), do: 0
 
   def of(i) do
-    Stream.iterate(1, &(&1 + 1))
+    integers()
     |> Enum.take(i)
-    |> Enum.reduce(&(&1+ &2))
+    |> Enum.reduce(&sum/2)
   end
+
+  defp integers(), do: Stream.iterate(1, &(&1 + 1))
+  defp sum(x, y), do: x + y
 end
